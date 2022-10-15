1cf92b24bc01187565ad1f540ddcdf7554640c6d
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-10-15 23:58:58 +1300

    Handle 404 and other errors

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,42 @@ namespace PixelBase.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult ServerError()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+
+  [Route("/Home/Error/{code:int}")]
+  public IActionResult Error(int code)
+  {
+    ViewData["ErrorCode"] = code;
+    switch (code)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      case 404:
+        ViewData["ErrorMsg"] = "Page not found";
+        break;
+      default:
+        ViewData["ErrorMsg"] = "Unknown";
+        break;
     }
+    return View("~/Views/Shared/Error.cshtml");
+  }
 }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/ServerError");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
+// app.UseStatusCodePagesWithReExecute("/Error/{0}");
+app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
