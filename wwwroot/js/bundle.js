ff5533fb1ebc662627da2724862987d9f1d4372b
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-10-15 22:16:08 +1300

    Basic homepage layout

--- wwwroot/js/bundle.js
+++ wwwroot/js/bundle.js
@@ -23,9 +23,10 @@ eval("/*!\n  * Bootstrap v5.2.2 (https://getbootstrap.com/)\n  * Copyright 2011-
 /*!*********************************!*\
   !*** ./wwwroot/scss/theme.scss ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Function rgb is missing argument $green.\\n        on line 33 of wwwroot/scss/theme.scss\\n>>     text-shadow: 1px 1px 2px rgb(0 0 0 / 80%);\\r\\n   -----------------------------^\\n\\n    at tryRunOrWebpackError (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5055:12)\\n    at __webpack_require__ (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5012:18)\\n    at F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5083:20\\n    at symbolIterator (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4990:43\\n    at symbolIterator (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Function rgb is missing argument $green.\\n        on line 33 of wwwroot/scss/theme.scss\\n>>     text-shadow: 1px 1px 2px rgb(0 0 0 / 80%);\\r\\n   -----------------------------^\\n\\n    at Object.<anonymous> (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!F:\\\\Development\\\\PixelBase\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[0].use[2]!F:\\\\Development\\\\PixelBase\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!F:\\\\Development\\\\PixelBase\\\\wwwroot\\\\scss\\\\theme.scss:1:7)\\n    at F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5057:39\\n    at tryRunOrWebpackError (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5055:12)\\n    at __webpack_require__ (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5012:18)\\n    at F:\\\\Development\\\\PixelBase\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5083:20\\n    at symbolIterator (F:\\\\Development\\\\PixelBase\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for F:\\\\Development\\\\PixelBase\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!F:\\\\Development\\\\PixelBase\\\\node_modules\\\\postcss-loader\\\\dist\\\\cjs.js??ruleSet[1].rules[0].use[2]!F:\\\\Development\\\\PixelBase\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!F:\\\\Development\\\\PixelBase\\\\wwwroot\\\\scss\\\\theme.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nSassError: Function rgb is missing argument $green.\\\\n        on line 33 of wwwroot/scss/theme.scss\\\\n>>     text-shadow: 1px 1px 2px rgb(0 0 0 / 80%);\\\\r\\\\n   -----------------------------^\\\\n\\\");\");\n\n//# sourceURL=webpack://pixelbase/./wwwroot/scss/theme.scss?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://pixelbase/./wwwroot/scss/theme.scss?");
 
 /***/ })
 
@@ -54,6 +55,18 @@ eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extrac
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
 /******/ 	
 /************************************************************************/
 /******/ 	
