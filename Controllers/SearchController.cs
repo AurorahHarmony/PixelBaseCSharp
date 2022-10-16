558638428df88ee44addde44e0f62cc692f3b1a2
Aurorah Harmony <aurorahHarmony@gmail.com>
2022-10-16 18:20:20 +1300

    Display assets on search page

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PixelBase.Models;
 
 namespace PixelBase.Controllers;
 
 public class SearchController : Controller
 {
-
-  public IActionResult Index()
+  private readonly PixelBaseContext _context;
+  public SearchController(PixelBaseContext context)
   {
-    return View();
+    _context = context;
+  }
+
+  public async Task<IActionResult> Index()
+  {
+    var assets = from a in _context.Asset select a;
+    return View(await assets.ToListAsync());
   }
 
 }
